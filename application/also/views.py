30e0a74a7c97951bd3eda2f85c33fe7dc57c6243
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-07-21 14:54:15 -0400

    edited the order again

--- application/also/views.py
+++ application/also/views.py
@@ -344,8 +344,8 @@ def simpleworklist(request):
 	categories = Category.objects.all()
 	work = categories.filter(slug = "work")
 
-	work = Article.objects.all().order_by('-date').filter(category=work,selected=False)
-	selected = Article.objects.all().order_by('-date').filter(selected=True)
+	work = Article.objects.all().order_by('order').filter(category=work,selected=False)
+	selected = Article.objects.all().order_by('order').filter(selected=True)
 
 	return render_to_response("archivelist.html",{"work":work,"selected":selected,"MEDIA_URL":settings.MEDIA_URL,"staff":request.user.is_staff})
 
